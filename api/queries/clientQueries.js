a5ecc04e4f8eca1066263b3ad842146ca6b1f231
Antonio Soares <soaresantonio28@gmail.com>
2018-06-15 00:01:03 -0300

    correção cadastro

--- api/queries/clientQueries.js
+++ api/queries/clientQueries.js
@@ -11,16 +11,29 @@ module.exports.getAllClients = function(req, res, next) {
 
 module.exports.getClientbyId = function(req,res,next){
     var id = parseInt(req.params.id);
+    client = {};
     res.locals.connection.query('SELECT * from clientes WHERE id = ' + id,
     function(error,results,fields){
         if (error){
             res.send(error);
             return;
         }
-        res.send(results);
+        client = results[0];
+        res.locals.connection.query('SELECT * from usuarios WHERE id = ' + client.user_id,
+        function(error,results,fields){
+            if (error){
+                res.send(error);
+                return;
+            }
+            client.nome = results[0].nome;
+            client.email = results[0].email;
+            client.senha = results[0].senha;
+            res.send(client)
+        });
+
     });
 };
-
+/*
 module.exports.createClient = function(req,res){
     const client = req.body;
     res.locals.connection.query("INSERT INTO clientes SET ?", client,
@@ -31,4 +44,21 @@ module.exports.createClient = function(req,res){
         }
         res.send(results);
     });
-};
+};*/
+
+module.exports.createClient = function(req,res){
+    const user = {"nome":req.body.nome,"email":req.body.email,"senha":req.body.senha};
+    const client = {"user_id":"","cpf":req.body.cpf};
+    res.locals.connection.query("INSERT INTO usuarios SET ?", user,
+    function(error, results, fields){
+            if (error){
+                res.send({ success: false, message: 'database error', error: error });
+                return;
+            }
+            client.user_id = results.insertId;
+            res.locals.connection.query("INSERT INTO clientes SET ?", client,
+            function(error,results,fields){
+                res.send({ success: true, messagem: 'Cliente cadastrado com sucesso', results:results });
+            });
+        });
+}
--- api/queries/workerQueries.js
+++ api/queries/workerQueries.js
@@ -22,6 +22,27 @@ module.exports.getWorkerbyId = function(req,res,next){
 };
 
 module.exports.createWorker = function(req,res){
+    const user = {"nome":req.body.nome,"email":req.body.email,"senha":req.body.senha};
+    const worker = {"user_id":"","cpf":req.body.cpf,"nota":0,"cnh":req.body.cnh};
+    res.locals.connection.query("INSERT INTO usuarios SET ?", user,
+    function(error, results, fields){
+        if(error){
+            res.send({sucess:false,message:'database error',error:error});
+            return;
+        }
+        worker.user_id = results.insertId;
+        res.locals.connection.query("INSERT INTO entregadores SET ?", worker,
+        function(error,results,fields){
+            if (error){
+                res.send(error);
+                return;
+            }
+            res.send({ success: true, messagem: 'Entregador cadastrado com sucesso', results:results });
+        });
+    });
+}    
+/*
+module.exports.createWorker = function(req,res){
     const worker = req.body;
     res.locals.connection.query("INSERT INTO entregadores SET ?", user,
     function(error,results,fields){
@@ -31,4 +52,4 @@ module.exports.createWorker = function(req,res){
         }
         res.send(results);
     });
-};
+};*/
