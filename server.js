69267ac08feadbd209e9602e196983e9fc77ec4e
Antonio Soares <soaresantonio28@gmail.com>
2018-07-04 13:48:58 -0300

    correção server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ require('./api/rotas/rotasProdutos')(app);
 require('./api/rotas/rotasListaDeCompra')(app);
    
 // Create a Server
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("App listening at http://%s:%s", host, port)
