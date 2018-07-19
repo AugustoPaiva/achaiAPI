15d92e5dca8353f35bb7e90ea9ac2a567ffe2688
AugustoPaiva <augustopaiva4@gmail.com>
2018-07-19 03:13:46 -0300

    minor change

--- api/controladores/controladorPedido.js
+++ api/controladores/controladorPedido.js
@@ -11,12 +11,9 @@ class ControladorPedido{
         var dia = ((dados.dataEntrega).split(" ")[0]).split("/");
         var hora = ((dados.dataEntrega).split(" ")[1]).split(":");
         
-        let data = new Date(dia[2],dia[1]-1,dia[0],1,5,1,10);
-        var s = new Date(1912,2,12,10,65,27,23);
-        console.log(s);
+        let data = new Date(dia[2],dia[1]-1,dia[0]);
         
-        
-        dados.dataEntrega = s;
+        dados.dataEntrega = data;
         return this.pedido.create(dados)
         .then(resposta => resposta )
         .catch(erro => {
@@ -24,15 +21,7 @@ class ControladorPedido{
         });
     }
 
-    //   adicionarPedido(dado){
-    //       return this.pedido.create(dado)
-    //       .then(resultado => resultado)
-    //       .catch(erro => erro);
-    //   }
 
-    //  retornarPedido(dado){
-    //      return this.pedido.sequelize.query('SELECT ')
-    //  }
 
 }
 module.exports = ControladorPedido;
